b589469c89f8a4b7241cb6536003277d75d5c278
Jana-script <jana.jscript@gmail.com>
2025-08-06 14:40:32 +0200

    first game script
    following the mouse

--- scenes/Main.cs
+++ scenes/Main.cs
@@ -1,19 +1,22 @@
 using Godot;
 using System;
 
+namespace Game;
+
 public partial class Main : Node2D
 {
+	private Sprite2D sprite;
+
 	public override void _Ready()
 	{
-		TestMethod();
-		GD.Print("Hello World");
+		sprite = GetNode<Sprite2D>("Cursor");
 	}
 
 	public override void _Process(double delta)
 	{
-	}
-	private void TestMethod()
-	{
-		GD.Print("entered test method");	
+		var mousePosition = GetGlobalMousePosition();
+		var gridPosition = mousePosition / 64;
+		gridPosition = gridPosition.Floor();
+		sprite.GlobalPosition = gridPosition * 64;
 	}
 }
