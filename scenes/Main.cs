8345ae69352216aba185fbc233050bb82239e4b2
Jana-script <jana.jscript@gmail.com>
2025-08-06 12:54:20 +0200

    C# and VSCode configuration
    +debugging

--- scenes/Main.cs
+++ scenes/Main.cs
@@ -5,10 +5,15 @@ public partial class Main : Node2D
 {
 	public override void _Ready()
 	{
+		TestMethod();
 		GD.Print("Hello World");
 	}
 
 	public override void _Process(double delta)
 	{
+	}
+	private void TestMethod()
+	{
+		GD.Print("entered test method");	
 	}
 }
