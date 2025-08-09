8653932381957f04e2361006f386f516a1b23bf7
Jana-script <jana.jscript@gmail.com>
2025-08-09 17:04:49 +0200

    Grid state for building rules
    using Grid state for more sophisticated building rules

--- scenes/Main.cs
+++ scenes/Main.cs
@@ -29,7 +29,7 @@ public partial class Main : Node
 
 	public override void _UnhandledInput(InputEvent evt)
 	{
-		if (hoveredGridCell.HasValue && evt.IsActionPressed("left_click") && gridManager.IsTilePositionValid(hoveredGridCell.Value))
+		if (hoveredGridCell.HasValue && evt.IsActionPressed("left_click") && gridManager.IsTilePositionBuildable(hoveredGridCell.Value))
 		{
 			PlaceBuildingAtHoveredCellPosition();
 			cursor.Visible = false;
@@ -55,10 +55,9 @@ public partial class Main : Node
 		AddChild(building);
 
 		building.GlobalPosition = hoveredGridCell.Value * 64;
-		gridManager.MarkTileAsOccupied(hoveredGridCell.Value);
 
 		hoveredGridCell = null;
-		// gridManager.ClearHighlight();
+		gridManager.ClearHighlightedTiles();
 	}
 
 	private void OnButtonPressed() 
